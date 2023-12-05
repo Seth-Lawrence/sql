a16e40c5e105b6ed423ac5a194832bae684300c9
Seth Hobson <sethlawrencehobson@gmail.com>
2023-12-05 15:25:55 -0500

    first 3 queries

--- queries.sql
+++ queries.sql
@@ -5,3 +5,18 @@
 FULL JOIN vehicles
 ON vehicles.owner_id = owners.id
 ORDER BY owners.id;
+
+
+
+SELECT first_name, COUNT(*) FROM owners
+INNER JOIN vehicles
+ON vehicles.owner_id = owners.id
+GROUP BY owners.id
+ORDER BY first_name;
+
+SELECT owners.first_name, ROUND(SUM(vehicles.price)/COUNT(*),2) AS average_price, COUNT(*) AS count
+FROM owners
+INNER JOIN vehicles ON vehicles.owner_id = owners.id
+GROUP BY owners.first_name
+HAVING COUNT(*) > 1 AND ROUND(SUM(vehicles.price)/COUNT(*),2) > 10000
+ORDER BY COUNT(*) DESC;
